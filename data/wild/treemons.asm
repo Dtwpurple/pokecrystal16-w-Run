814583db8621880d99c0125789788937b3381af6
Dtwpurple <dylanwiillyams@gmail.com>
2026-05-03 22:45:11 -0700

    Update treemons.asm

--- data/wild/treemons.asm
+++ data/wild/treemons.asm
@@ -180,8 +180,8 @@ TreeMonSet_BeachRock: ; Route 40, Cianwood City
 	dbbw 30, 20, KRABBY
 	dbbw 30, 20, DWEBBLE
 	dbbw 30, 20, BINACLE
-	dbbw 10, 20, SHUCKLE
-    dbbw 12, 18, ROLYCOLY
+	dbbw 9, 20, SHUCKLE
+    dbbw 1, 18, ROLYCOLY
 	db -1
 
 TreeMonSet_KantoRock: ; Victory Road
